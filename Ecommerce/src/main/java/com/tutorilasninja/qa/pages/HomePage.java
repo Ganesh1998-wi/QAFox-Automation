cf4d0bd4b58a4b8b94d9c92338c9790a2e027f95
Delhi Ganesh <68279174+Ganesh1998-wi@users.noreply.github.com>
2024-02-26 01:01:51 +0530

    pages got commited

--- Ecommerce/src/main/java/com/tutorilasninja/qa/pages/HomePage.java
+++ Ecommerce/src/main/java/com/tutorilasninja/qa/pages/HomePage.java
@@ -1,10 +1,22 @@
 package com.tutorilasninja.qa.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	WebDriver driver;
+	
+	@FindBy(xpath = "//li[@class='dropdown']/a[@title='My Account']")
+	private WebElement MyAccountButton;
+	
+	@FindBy(xpath = "//ul[contains(@class,'dropdown-menu')]//li/a[text()='Login']")
+	private WebElement LoginButton;
+	
+	@FindBy(xpath = "//ul[contains(@class,'dropdown-menu')]//li/a[text()='Register']")
+	private WebElement RegisterButton;
+	
 
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
