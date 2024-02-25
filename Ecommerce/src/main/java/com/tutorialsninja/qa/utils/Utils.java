1a0cfa13830a3771c2ed15f5e6a940b6cbe3b77c
Delhi Ganesh <68279174+Ganesh1998-wi@users.noreply.github.com>
2024-02-25 22:38:26 +0530

    MyfirstProject

---
+++ Ecommerce/src/main/java/com/tutorialsninja/qa/utils/Utils.java
@@ -0,0 +1,62 @@
+package com.tutorialsninja.qa.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import io.cucumber.java.sl.Ce;
+
+public class Utils {
+
+	public String DateToAppend() {
+		Date date = new Date();
+		String EmailId = date.toString().replace(":", "_").replace(" ", "_");
+		return "delhiganeshv" + EmailId + "@gmail.com";
+	}
+
+	public static final int IMPLICIT_WAIT_TIME = 10;
+	public static final int PAGELOAD_TIME_OUT = 10;
+
+	public static Object[][] ReadDataFromExcel(String SheetName) {
+		XSSFWorkbook workbook = null;
+		File Excelfile = new File(
+				System.getProperty("user.dir") + "\\src\\main\\java\\com\\tutorialsninja\\qa\\testdata\\TestData.xlsx");
+		try {
+			FileInputStream excelstream = new FileInputStream(Excelfile);
+			workbook = new XSSFWorkbook(excelstream);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		XSSFSheet sheet = workbook.getSheet("Login");
+		int row = sheet.getLastRowNum();
+		int cols = sheet.getRow(0).getLastCellNum();
+		Object[][] data = new Object[row][cols];
+		for (int i = 0; i < row; i++) {
+			XSSFRow rows = sheet.getRow(i + 1);
+			for (int j = 0; j < cols; j++) {
+				XSSFCell cell = rows.getCell(j);
+				CellType celltype = cell.getCellType();
+				switch (celltype) {
+				case STRING:
+					data[i][j] = cell.getStringCellValue();
+					break;
+				case NUMERIC:
+					data[i][j] = Integer.toString((int) cell.getNumericCellValue());
+					break;
+				case BOOLEAN:
+					data[i][j] = cell.getBooleanCellValue();
+					break;
+				}
+			}
+
+		}
+		return data;
+	}
+
+}
---
+++ Ecommerce/src/test/java/com/tutorialsninja/qa/base/BaseClass.java
@@ -0,0 +1,63 @@
+package com.tutorialsninja.qa.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.tutorialsninja.qa.utils.Utils;
+
+public class BaseClass {
+	WebDriver driver;
+	Properties prop;
+	Properties testprop;
+
+	public BaseClass() {
+		prop = new Properties();
+		File propfile = new File(System.getProperty("user.dir")
+				+ "\\src\\main\\java\\com\\tutorialsninja\\qa\\properties\\config.properties");
+		try {
+			FileInputStream propinputfile = new FileInputStream(propfile);
+			prop.load(propinputfile);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+		testprop = new Properties();
+		File proptestfile = new File(System.getProperty("user.dir")
+				+ "\\src\\main\\java\\com\\tutorialsninja\\qa\\testdata\\testdata.properties");
+		try {
+			FileInputStream propinputtestfile = new FileInputStream(proptestfile);
+			prop.load(propinputtestfile);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	public WebDriver LaunchTheDriver(String BrowserName) {
+		if (BrowserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"D:\\Automation\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("D:\\Automation\\chrome-win64\\chrome-win64\\chrome.exe");
+			driver = new ChromeDriver(options);
+		} else if (BrowserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else if (BrowserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		driver.get(prop.getProperty("URL"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utils.IMPLICIT_WAIT_TIME));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Utils.PAGELOAD_TIME_OUT));
+		return driver;
+
+	}
+
+}
