25d732b1ed2595ef1dd6af5d6029f49bedbf92f6
Delhi Ganesh <68279174+Ganesh1998-wi@users.noreply.github.com>
2024-02-26 09:22:44 +0530

    Pages commited

--- Ecommerce/src/main/java/com/tutorilasninja/qa/pages/RegisterPage.java
+++ Ecommerce/src/main/java/com/tutorilasninja/qa/pages/RegisterPage.java
@@ -1,10 +1,34 @@
 package com.tutorilasninja.qa.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegisterPage {
 	WebDriver driver;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/input[@name='firstname']")
+	private WebElement FirstName;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/input[@name='lastname']")
+	private WebElement LastName;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/input[@name='email']")
+	private WebElement Email;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/input[@name='telephone']")
+	private WebElement Telephone;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/input[@name='password']")
+	private WebElement Password;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/input[@name='confirm']")
+	private WebElement ConfirmPassword;
+	
+	@FindBy(xpath = "//div[contains(@class,'col-sm-10')]/label/input[@value=1]")
+	private WebElement NewsLetter;
+	
 
 	public RegisterPage(WebDriver driver) {
 		this.driver = driver;
