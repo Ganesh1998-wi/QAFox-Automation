1f68f1f38f34551119ce60ea3618fa854b015d82
Delhi Ganesh <68279174+Ganesh1998-wi@users.noreply.github.com>
2024-02-26 08:15:48 +0530

    Pages Updated

--- Ecommerce/src/main/java/com/tutorilasninja/qa/pages/LoginPage.java
+++ Ecommerce/src/main/java/com/tutorilasninja/qa/pages/LoginPage.java
@@ -1,10 +1,18 @@
 package com.tutorilasninja.qa.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	WebDriver driver;
+	
+	@FindBy(xpath = "//div[contains(@class,'form-group')]//input[@id='input-email']")
+	private WebElement EmailAddress;
+	
+	@FindBy(xpath = "//div[contains(@class,'form-group')]//input[@id='input-password']")
+	private WebElement Password;
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
