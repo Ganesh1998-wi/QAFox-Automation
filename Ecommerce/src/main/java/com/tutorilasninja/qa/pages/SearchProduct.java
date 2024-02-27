cca83cacf70a53fe356331d761b7639a9e1b29c1
Delhi Ganesh <68279174+Ganesh1998-wi@users.noreply.github.com>
2024-02-27 19:15:30 +0530

    All Pages Got commited

--- Ecommerce/src/main/java/com/tutorilasninja/qa/pages/SearchProduct.java
+++ Ecommerce/src/main/java/com/tutorilasninja/qa/pages/SearchProduct.java
@@ -7,11 +7,25 @@ import org.openqa.selenium.support.PageFactory;
 
 public class SearchProduct {
 	WebDriver driver;
-	
 
+	@FindBy(xpath = "//div[contains(@class,'caption')]/h4/a[text()='HP LP3065']")
+	private WebElement ValidProduct;
+
+	@FindBy(xpath = "//p[text()='There is no product that matches the search criteria.']")
+	private WebElement InvalidProduct;
 
 	public SearchProduct(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
+	}
+
+	public String GotTheValidProduct() {
+		String validlaptop = ValidProduct.getText();
+		return validlaptop;
+	}
+
+	public String GotTheInvalidProduct() {
+		String InvalidSearch = InvalidProduct.getText();
+		return InvalidSearch;
 	}
 }
